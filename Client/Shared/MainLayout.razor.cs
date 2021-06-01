94892abcd871f27083b6b99222e492c9816287e3
Arend-melissant <arend.melissant@gmail.com>
2021-06-01 21:08:11 +0200

    balzority

--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -2,20 +2,11 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Radzen;
-using Radzen.Blazor;
 
 namespace BlazorWebasmHosted.Client.Shared
 {
     public partial class MainLayoutComponent
     {
-        protected async System.Threading.Tasks.Task SidebarToggle0Click(dynamic args)
-        {
-            await InvokeAsync(() => { sidebar0.Toggle(); });
-
-            await InvokeAsync(() => { body0.Toggle(); });
-        }
-
         public string ErrorButtonText {get; set;} = "Set Error";
 
         protected async System.Threading.Tasks.Task ShowError()
